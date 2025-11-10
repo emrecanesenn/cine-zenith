d380597debfd767f5b70815c43a13c1493a51e3a
Schedarp <emrecannesen@gmail.com>
2025-11-10 15:46:23 +0300

    Update 0.1

---
+++ assets/js/movieScript.js
@@ -0,0 +1,124 @@
+"use strict";
+
+import {API_KEY, DEFAULT_URL, IMG_DEFAULT_URL, LANG_TR} from './apiSettings.js';
+
+
+// Essentials List Movies
+async function listMovies() {
+    try {
+
+        const resolve = await fetch(`${DEFAULT_URL}/movie/upcoming?api_key=${API_KEY}`);
+        if (!resolve.ok) throw new Error("Upcoming Movies is not loaded!");
+
+        const data = await resolve.json();
+
+        await heroSection(data.results)
+        await upcomingSection(data.results)
+
+    } catch (e) {
+        alert(`HATA: ${e}`)
+    }
+
+}
+
+// HERO SECTION FIRST UPCOMING FILM
+async function heroSection(upcoming) {
+    try {
+
+        const resolve = await fetch(`${DEFAULT_URL}/movie/${upcoming[0].id}?api_key=${API_KEY}`)
+        if (!resolve.ok) throw new Error(`The movie is not uploading`)
+        const firstUpcoming = await resolve.json();
+
+        // DOM Variables
+        const title = document.getElementById("hero-title")
+        const tagline = document.getElementById("hero-tagline")
+        const genres = document.getElementById("hero-genres")
+        const releaseDate = document.getElementById("hero-release-date")
+        const runtime = document.getElementById("hero-runtime")
+        const heroBackground = document.getElementById("heroSection");
+
+        // Hero Displaying Settins
+        title.innerHTML = firstUpcoming.title;
+        tagline.innerHTML = firstUpcoming.tagline || `This <strong>Movie</strong>, will be released soon.`;
+        releaseDate.innerHTML = firstUpcoming.release_date.slice(0, 4);
+        runtime.innerHTML = `${firstUpcoming.runtime} min`;
+
+        // Genres Displaying
+        genres.innerHTML = "";
+        const genreNames = firstUpcoming.genres.map(genre => genre.name);
+        genreNames.sort()
+
+        for (let x = 0; x < genreNames.length; x++) {
+            if (x === ( genreNames.length - 1 ))
+                genres.innerHTML += `<a href="#">${genreNames[x]}</a>`; // Last genre
+                else
+                    genres.innerHTML += `<a href="#">${genreNames[x]},</a>`;
+        }
+
+        // Hero Background Settings
+        heroBackground.style.background = `url("${IMG_DEFAULT_URL}original${firstUpcoming.backdrop_path}")`;
+        heroBackground.style.backgroundSize = 'cover';
+        heroBackground.style.backgroundPosition = 'center';
+
+
+    } catch (e) {
+        alert(`HATA: ${e}`)
+    }
+}
+
+async function upcomingSection(upcomings) {
+    const movieList = document.getElementById("upcoming-movie-list")
+    const upcoming =upcomings.slice(0, 5);
+    try {
+        for (const movie of upcoming) {
+            const resolve = await fetch(`${DEFAULT_URL}/movie/${movie.id}?api_key=${API_KEY}`)
+            if (!resolve.ok) throw new Error("Upcoming listing error")
+            const movieDetails = await resolve.json()
+
+            const li = document.createElement("li")
+            li.innerHTML = `
+                <div class="movie-card">
+
+                      <a href="movie-details.html">
+                          <figure class="card-banner">
+                              <img src="${IMG_DEFAULT_URL}original/${movieDetails.poster_path}" alt="${movieDetails.title} poster">
+                          </figure>
+                      </a>
+
+                      <div class="title-wrapper">
+                          <a href="movie-details.html">
+                              <h3 class="card-title">${movieDetails.title}</h3>
+                          </a>
+
+                          <span>${movieDetails.release_date.slice(0, 4)}</span>
+                      </div>
+
+                      <div class="card-meta">
+                          <div class="badge badge-outline">HD</div>
+
+                          <div class="duration">
+                              <ion-icon name="time-outline"></ion-icon>
+
+                              <span>${movieDetails.runtime} min</span>
+                          </div>
+
+                          <div class="rating">
+                              <ion-icon name="star"></ion-icon>
+
+                              <data>${movieDetails.vote_average.toFixed(1)}</data>
+                          </div>
+                      </div>
+
+                  </div>
+        `
+            movieList.appendChild(li)
+        }
+    } catch (error) {
+        alert(`HATA: ${error}`)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    listMovies();
+})
+//
---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+'use strict';
+/**
+ * Components Fetching
+ */
+
+async function compFetch(sectionID, filePath) {
+
+  try {
+
+    const section = document.getElementById(sectionID)
+
+    const resolve = await fetch(filePath)
+    if (!resolve.ok) throw new Error("Dosya düzgün yüklenemedi!");
+
+    section.innerHTML = await resolve.text();
+
+  } catch (err) {
+    console.error(`HATA: ${err}`);
+    alert(`HATA: ${err}`);
+  }
+}
+
+async function initializeApp() {
+  const loader = document.getElementById('page-loader');
+
+  await Promise.all([
+    compFetch("footer", `assets/components/footer.html`),
+    compFetch("header", `assets/components/header.html`),
+    compFetch("menu", `assets/components/menu.html`)
+  ]);
+
+
+  /**
+   * navbar variables
+   */
+  const navOpenBtn = document.getElementById("data-menu-open-btn");
+  const navCloseBtn = document.getElementById("data-menu-close-btn");
+  const navbar = document.getElementById("data-navbar");
+  const overlay = document.getElementById("data-overlay");
+  const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+  for (let i = 0; i < navElemArr.length; i++) {
+
+    navElemArr[i].addEventListener("click", function () {
+
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+      document.body.classList.toggle("active");
+
+    });
+
+  }
+
+  /**
+   * header sticky
+   */
+  const header = document.getElementById("menu");
+
+  window.addEventListener("scroll", function () {
+
+    window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+  });
+
+
+  /**
+   * go top
+   */
+
+  const goTopBtn = document.getElementById("data-go-top");
+
+  window.addEventListener("scroll", function () {
+
+    window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+  });
+
+
+  // 3. Yükleyiciyi Gizle
+  setTimeout(() => {
+    if (loader) {
+      loader.classList.add('hidden'); // DOM'dan tamamen kaldırarak sayfa düzenini optimize et
+    }
+  }, 500);
+
+  // 4. Ana Uygulama Mantığını Başlat
+  console.log("Cine-Zenith hazır ve çalışıyor!");
+}
+
+document.addEventListener("DOMContentLoaded", initializeApp)
