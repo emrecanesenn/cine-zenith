034b11b087d1a3d74c2c496ef00760d653031be5
Schedarp <emrecannesen@gmail.com>
2025-11-10 16:05:33 +0300

    apiSettings

---
+++ assets/js/apiSettings.js
@@ -0,0 +1,5 @@
+// API-SETTINGS
+export const API_KEY = 'ef118284aeb4d24b39c622a5c9132db9';
+export const LANG_TR = '&language=tr-TR';
+export const DEFAULT_URL = 'https://api.themoviedb.org/3';
+export const IMG_DEFAULT_URL = 'https://image.tmdb.org/t/p/';
